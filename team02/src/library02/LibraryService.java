05237383cef55097b11d8283a68e24c23294d0c2
gisu-95 <z0170z@naver.com>
2025-07-09 16:08:28 +0900

    250709 16:07 김기수 libraryservice 첫커밋

--- team02/src/library02/LibraryService.java
+++ team02/src/library02/LibraryService.java
@@ -1,5 +1,42 @@
 package library02;
 
-public class LibraryService {
+import java.util.ArrayList;
+import java.util.List;
 
+//기수 LibraryService
+//리스트<Borrow> = <Book>//업캐스팅
+//
+//기본생성자   
+//   리스트<Borrow> = <Book>
+//
+//List<String> getBookList()
+//boolean 대여(Member, 도서제목) throws 대출여부에따라 예외처리; 
+//   book -> 대여 호출
+//   member -> 대여 호출
+//boolean 반납(Member, 도서제목) book -> 반납 호출
+//   book -> > 반납 호출
+//   member -> 반납 호출
+public class LibraryService {
+	List<Borrow> libraryService;
+
+	public LibraryService(List<Borrow> libraryService) {
+		super();
+		libraryService = new ArrayList<Borrow>(); // 상속하면 Borrow -> Book 으로 바꿔
+		this.libraryService = libraryService;
+	}
+	
+	
+	
+	
+	public boolean rentalBook(Member, name) throws Exception{
+//		멤버 -> 19개 미만이면 대여가능 20개 이상이면 대여 불가능
+//		책 -> fasle 면 대여가능 true면 대여 불가능
+//		Member(name)
+	}
+	
+	public boolean returnBook(Member, name) {
+//		
+		
+	}
+	
 }
