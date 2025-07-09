b1285b4421346101bff77175bb8f6efe602d9899
boraJun <qhfk7751@naver.com>
2025-07-09 12:42:23 +0900

    250709 12:41 pull 받기전 커밋

--- team02/src/library02/User.java
+++ team02/src/library02/User.java
@@ -1,5 +1,12 @@
 package library02;
 
 public abstract class User {
-
+	//사용자 기본정보 필드
+		private String id;
+		private String password;
+		private String name;
+		
+		abstract boolean login(String id, String pw);
+		
+		
 }
