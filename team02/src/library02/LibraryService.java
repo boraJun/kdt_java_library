e13f145380b54fb67b30b18a9223b379e3aec4ba
gisu-95 <z0170z@naver.com>
2025-07-09 19:22:21 +0900

    250709 19:21 김기수 libraryService 4번째커밋

--- team02/src/library02/LibraryService.java
+++ team02/src/library02/LibraryService.java
@@ -6,68 +6,66 @@ import java.util.List;
 //기수 LibraryService
 public class LibraryService {
 //리스트<Borrow> = <Book>//업캐스팅
-	private List<Book> bookList;
+	private List<Borrow> bookList;
 
 //기본생성자   
-public LibraryService(List<Book> libraryService) {
-	super();
-	bookList = new ArrayList<Book>();// 상속하면 Borrow -> Book 으로 바꿔
-	this.bookList = libraryService;
-}
+	public LibraryService(List<Borrow> bookList) {
+		super();
+		this.bookList = new ArrayList<Borrow>();// 상속하면 Borrow -> Book 으로 바꿔
+		this.bookList = bookList;
+	}
 
 //도서목록 출력 메소드
 //List<String> getBookList()
-	public List<String> getBookList(){
-		List<String> bl = new  ArrayList<>();
-		for(Book b : bookList) {
-			bl.add(b.getName());
-			
+	public List<String> getBookList() {
+		List<String> bl = new ArrayList<>();
+		for (Borrow b : bookList) {
+			if (b instanceof Book) {
+				bl.add(((Book) b).getName());
+			}
 		}
 		return bl;
 	}
-	
+
 //도서 검색 메소드
-	public boolean searchBook(String bookname) {
-		if(bookList == null)
-			return false;
-		
-		for(Book b : bookList) {
-			//if(b.getName().equals(bookName))
-				return true;
+	public Book searchBook(String bookName) {
+		if (bookList == null)
+			return null;
+
+		for (Borrow b : bookList) {
+			if (b instanceof Book) {
+
+				if (((Book) b).getName().equals(bookName))
+
+					return (Book) b;
+			}
 		}
-		
-		return false;
+
+		return null;
 	}
-	
-	
+
 //boolean 대여(Member, 도서제목) throws 대출여부에따라 예외처리; 
 //   book -> 대여 호출
 //   member -> 대여 호출
-	public boolean rentalBook(Member, ) throws Exception{
-//		멤버 -> 19개 미만이면 대여가능 20개 이상이면 대여 불가능 맥스바로우
-//		if(Member(책빌린개수)<19) {
-//			대여가능
-//		}else {
-//			대여불가능
-//		}
-//		책 -> fasle 면 대여가능 true면 대여 불가능 북 낫 어베일러블
-//		Member(name)
-//		if(Member(name) = false) {
-//			대여가능
-//		}else {
-//			대여불가능
-//		}
-		return;
+	public boolean rentalBook(Member m, String bookName) throws MaxBorrowException, BookNotAvailableException {
+		if (this.searchBook(bookName).borrowBook()) {
+			m.borrowingBook(bookName);
+			return true;
+		}
+
+		return false;
 	}
-	
+
 //boolean 반납(Member, 도서제목) book -> 반납 호출
 //   book -> > 반납 호출
 //   member -> 반납 호출
-	public boolean returnBook(Member, ) {
-//		흠
-		
-		
-		return;
+	public boolean returnBook(Member m, String bookName) {
+		if (m.returningBook(bookName)) {
+			this.searchBook(bookName).returnBook();
+			return true;
+		}
+
+		return false;
 	}
-	
+
 }
