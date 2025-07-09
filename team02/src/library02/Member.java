d38f96b9a153a7dd4df9d618869fb68a97d164a7
boraJun <qhfk7751@naver.com>
2025-07-09 16:11:56 +0900

    250709 jbr member.java 잠시 수정

--- team02/src/library02/Member.java
+++ team02/src/library02/Member.java
@@ -1,5 +1,16 @@
 package library02;
 
-public class Member {
+public class Member extends User{
+
+	public Member(String id, String password, String name) {
+		super(id, password, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean login(String id, String pw) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
